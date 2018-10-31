ee1c185b81af4fb66c74871c1fa294456da27a77
algreen11 <AlexT@alexs-mbp-3.colgate.edu>
2018-10-30 20:56:13 -0400

    implemented RSI

---
+++ Implementation/algorithmictrading/strategy/relativestrengthindex.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from algorithmictrading.stockData.getStock import stockDataRetriever
+
+
+def execute(stock_name, start_date, end_date, fetchStocks):
+    stock = stockDataRetriever(stock_name, start_date, end_date).fetchStock(fetchStocks)
+
+    df = pd.DataFrame(index=stock.index)
+    df['Close'] = stock['Close'].copy()
+    df['buy_signal'] = 0
+    df['sell_signal'] = 0
+    window = 14
+
+    # create frames of roll ups and roll downs
+    delta = stock['Close'].diff()
+    upgains, downloss = delta.copy(), delta.copy()
+    upgains[upgains < 0] = 0
+    downloss[downloss > 0] = 0
+
+    # use moving average to create roll up/down
+    roll_up = upgains.rolling(window=window, min_periods=1, center=False).mean()
+    roll_down = downloss.abs().rolling(window=window, min_periods=1, center=False).mean()
+
+    # create RSI
+    RS = roll_up/roll_down
+    RSI = 100.0 - (100.0 / (1.0 + RS))
+
+    # plot RSI
+    fig = plt.figure()
+    fig.suptitle(stock_name + ": RSI")
+    RSI.plot()
+
+    df['RSI'] = RSI.copy()
+
+    # Create df for buy and sell signal
+    df['buy_signal'][window:] = np.where(df['RSI'][window:] < 30, 1.0, 0)
+    df['sell_signal'][window:] = np.where(df['RSI'][window:] > 70, 0, 1.0)
+
+    # when signal changes from 1 to 0 or 0 to 1 - is a buy or sell
+    df['buy_positions'] = df['buy_signal'].diff()
+    df['sell_positions'] = df['sell_signal'].diff()
+
+    # merge positions of buying - when RSI crosses 30 and selling - when price crosses 70
+    df['buy_positions'] = np.where(df['buy_positions'] < 0, 0, df['buy_positions']) 
+    df['sell_positions'] = np.where(df['sell_positions'] > 0, 0, df['sell_positions'])
+    df['positions'] = df['buy_positions'] + df['sell_positions']
+
+    df['signal'] = createSignals(df['positions'].copy())
+
+    ax1 = plt.figure().add_subplot(111,  ylabel='Price in $')
+    df[['Close']].plot(ax=ax1, lw=2.)
+    ax1.set_title(stock_name[5:] + ": RSI")
+    ax1.plot(df.loc[df.positions == 1.0].index,
+             df.Close[df.positions == 1.0],
+             '^', markersize=10, color='m')
+    ax1.plot(df.loc[df.positions == -1.0].index,
+             df.Close[df.positions == -1.0],
+             'v', markersize=10, color='k')
+
+    share_amount = 100
+    initial_capital = float(100000.0)
+
+    positions = pd.DataFrame(index=df.index).fillna(0.0)
+    positions[stock_name] = share_amount*df['signal']
+    portfolio = positions.multiply(stock['Adj. Close'], axis=0)
+    pos_diff = positions.diff()
+
+    portfolio['holdings'] = (positions.multiply(stock['Adj. Close'], axis=0)).sum(axis=1)
+    portfolio['cash'] = initial_capital - (pos_diff.multiply(stock['Adj. Close'], axis=0)).sum(axis=1).cumsum()
+    portfolio['total'] = portfolio['cash'] + portfolio['holdings']
+    portfolio['returns'] = portfolio['total'].pct_change()
+
+    # compare against baseline long position
+    baseline_profit = (stock['Adj. Close'].iloc[-1] - stock['Adj. Close'].iloc[0])*share_amount
+    strategy_profit = portfolio['total'].iloc[-1] - initial_capital
+    print(baseline_profit, strategy_profit)
+    if strategy_profit < baseline_profit:
+        percentage_difference_profit = round((float(strategy_profit - abs(baseline_profit))) / initial_capital * 100, 2)
+    else:
+        percentage_difference_profit = round((float(strategy_profit - baseline_profit)) / initial_capital * 100, 2)
+    return percentage_difference_profit, plt
+
+
+# function that creates stock ownership signals from positions
+def createSignals(positions):
+    signals = positions
+    for x in range(1, len(signals)):
+        if (signals[x] == 0):
+            if (signals[x-1] == 1):
+                signals[x] = 1
+        elif (signals[x] == -1):
+            signals[x] = 0
+    return signals
--- Implementation/driver.py
+++ Implementation/driver.py
@@ -1,4 +1,4 @@
-from algorithmictrading.strategy import sma, ema, bollingerbands, pairstrading
+from algorithmictrading.strategy import sma, ema, bollingerbands, pairstrading, relativestrengthindex
 
 stock_tickers = ["WIKI/AAPL", "WIKI/GOOG", "WIKI/DIS", 'WIKI/FB', 'WIKI/INTC', 'WIKI/MSFT', 'WIKI/AMD', 'WIKI/HAS', 'WIKI/EBAY', 'WIKI/AAL', 'WIKI/PYPL', 'WIKI/QCOM', 'WIKI/SBUX', 'WIKI/VOD', 'WIKI/MNST']
 fetch_stocks = False  # if array of stockTickers updated run driver with this set to true, will download csv
@@ -17,9 +17,13 @@ def momentum_strategies():
         plot, percent_performance_bbands = bollingerbands.execute(stock, "2006-10-01", "2017-01-01", fetch_stocks)
         plots.append(plot)
 
+        percent_performance_rsi, plot = relativestrengthindex.execute(stock, "2006-10-01", "2017-01-01", fetch_stocks)
+        plots.append(plot)
+
         print(stock, "SMA", percent_performance_sma)
         print(stock, "EMA", percent_performance_ema)
         print(stock, "Bollinger Bands", percent_performance_bbands)
+        print(stock, "RSI", percent_performance_rsi)
 
     # for plot in plots:
     #     plot.show()
@@ -55,9 +59,7 @@ def pairs_cointegration():
     # ('WIKI/AMD', 'WIKI/EBAY', 0.027462171694469979)
     # ('WIKI/AMD', 'WIKI/VOD', 0.037477501569076914)
 
-
-
 if __name__ == '__main__':
-    #momentum_strategies()
-    pairs_strategy()
+    momentum_strategies()
+    #pairs_strategy()
     #pairs_cointegration()
